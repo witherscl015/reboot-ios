3f88824a3b4495353a7e81f5e127c659b065c1ea
Jamone Kelly <xg.monie089@gmail.com>
2018-03-24 11:38:04 -0400

    initial commit

--- Reboot/ViewController.swift
+++ Reboot/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class Onboarding: UIViewController {
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var signInBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Styling the sign up button
+        self.signUpBtn.layer.cornerRadius = 10.0
+        self.signUpBtn.layer.borderWidth = 0.8
+        self.signUpBtn.layer.borderColor = UIColor.green.cgColor
+        self.signUpBtn.setTitle("Sign Up", for: .normal)
+        self.signUpBtn.setTitleColor(UIColor.black, for: .normal)
+        
+        // Styling the sign in button
+        self.signInBtn.layer.cornerRadius = 10.0
+        self.signInBtn.layer.borderWidth = 0.8
+        self.signInBtn.layer.borderColor = UIColor.green.cgColor
+        self.signInBtn.setTitle("Sign In", for: .normal)
+        self.signInBtn.setTitleColor(UIColor.black, for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
