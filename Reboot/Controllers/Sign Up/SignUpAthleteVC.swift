878208491da36eafed599e9ae3881f2568c6dfdf
Jamone Kelly <jamone.kelly@gmail.com>
2020-08-06 21:09:56 -0400

    Update project

--- Reboot/Controllers/Sign Up/SignUpAthleteVC.swift
+++ Reboot/Controllers/Sign Up/SignUpAthleteVC.swift
@@ -7,7 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpAthleteVC: UIViewController {
+    let schoolList = ["South Rowan High School", "North Rowan High School", "East Rowan High School", "West Rowan High School"]
+    @IBOutlet weak var schoolPickerView: UIPickerView!
+    @IBOutlet weak var coachPickerView: UIPickerView!
+    @IBOutlet weak var fullNameTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.schoolPickerView.delegate = self
+        self.schoolPickerView.dataSource = self
+    }
+}
+
+extension SignUpAthleteVC: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return schoolList.count
+    }
+}
+
+extension SignUpAthleteVC: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return schoolList[row]
+    }
 }
--- Reboot/Controllers/Sign Up/SignUpCoachVC.swift
+++ Reboot/Controllers/Sign Up/SignUpCoachVC.swift
@@ -7,7 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpCoachVC: UIViewController {
+    let schoolList = ["South Rowan High School", "North Rowan High School", "East Rowan High School", "West Rowan High School"]
+    @IBOutlet weak var schoolPickerView: UIPickerView!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Create Account", style: .done, target: self, action: nil)
+        
+        self.schoolPickerView.dataSource = self
+        self.schoolPickerView.delegate = self
+    }
+}
+
+extension SignUpCoachVC: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return schoolList[row]
+    }
+}
+
+extension SignUpCoachVC: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return schoolList.count
+    }
 }
